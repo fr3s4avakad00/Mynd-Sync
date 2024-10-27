ad8b378cfce48cefb86a05fbb598c9fcaca874d0
thickCode <118022331+fr3s4avakad00@users.noreply.github.com>
2024-10-27 14:46:15 -0400

    Update focusGameMenu.js

--- focusGameMenu.js
+++ focusGameMenu.js
@@ -3,5 +3,12 @@ function startGame() {
 }
 
 function instructions() {
-    window.location.href = "instructions.html";
+   alert(
+       "Instructions \n\n" +
+       "1. Sign Up or Log In: On first use, sign up on the sign-up page to create a profile. Returning users can log in on the loadgame page. \n\n" +
+       "2. Customize Character: After logging in, proceed to Character Customization. Navigate to select hair, face, and body features for the character. \n\n" +
+       "3. Start Focus Session: Navigate to the timer page to start a focus timer. When the session ends, points are awarded based on time spent. \n\n" +
+       "4. Earn and Spend Points: Earn points through focus sessions and spend them on customizations like background and items in the shop. \n\n" +
+       "5. Logout and Reset: Users can log out or reset data on gamePage.html through the Log Out and Reset buttons."
+   )
 }
